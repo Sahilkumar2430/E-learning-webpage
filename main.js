2e365c20ca77bd2ecdf9899973f4664fb3816662
sahil kumar <sonuyadavrajeshkumar@gmail.com>
2025-07-06 07:56:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+document.getElementById('menuToggle').addEventListener('click', function () {
+  document.getElementById('navLinks').classList.toggle('show');
+});
+
+
+console.log("hello world");
